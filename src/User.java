3d71e2020695ea5a0987755b09bfd9f73d31dcee
songuimpale <songfreddy07@gmail.com>
2024-01-31 16:17:46 +0100

    Add User Form

---
+++ src/User.java
@@ -0,0 +1,28 @@
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters et setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
+
